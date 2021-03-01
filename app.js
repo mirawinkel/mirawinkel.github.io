889bf5eccc8748fd269b25ed4687215ef8449f86
mirawinkel <mirawinkel206@gmail.com>
2021-03-01 00:48:58 -0800

    add animated tile on bottom of index

---
+++ app.js
@@ -0,0 +1,15 @@
+let elementsArray = document.querySelectorAll(".tile");
+console.log(elementsArray);
+window.addEventListener('scroll', fadeIn ); 
+function fadeIn() {
+    for (var i = 0; i < elementsArray.length; i++) {
+        var elem = elementsArray[i]
+        var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+        if (distInView < 0) {
+            elem.classList.add("inView");
+        } else {
+            elem.classList.remove("inView");
+        }
+    }
+}
+fadeIn();
