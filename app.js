e15760d60ac61eb91d75a8c5366a6a8aaab63a4a
mirawinkel <mirawinkel206@gmail.com>
2021-03-01 18:42:21 -0800

    added on scroll animations to text

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-let elementsArray = document.querySelectorAll(".tile");
+const elementsArray = document.querySelectorAll(".tile");
+const textArray = document.querySelectorAll(".textFade");
 console.log(elementsArray);
 window.addEventListener('scroll', fadeIn ); 
 function fadeIn() {
-    for (var i = 0; i < elementsArray.length; i++) {
-        var elem = elementsArray[i]
-        var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+    for (let i = 0; i < elementsArray.length; i++) {
+        let elem = elementsArray[i]
+        let distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
         if (distInView < 0) {
             elem.classList.add("inView");
         } else {
